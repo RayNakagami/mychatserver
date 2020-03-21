2323c7da7a7323fe1510fad89166483e3cead095
RayN <ray.nakagami@gmail.com>
2020-03-21 17:23:24 +0900

    save message on db

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 # server.py
 from flask import Flask, request, render_template
+from falsk_login import current_user
 from gevent import pywsgi
 from geventwebsocket.handler import WebSocketHandler
+
 import json
 import socket
 import sqlite3
@@ -9,10 +11,13 @@ import sqlite3
 app = Flask(__name__)
 app.config.from_object(__name__)
 clients = {}
+conn = sqlite3.connect('message.db')
+ 
+ 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("chat.html")
 
 
 @app.route("/pipe")
@@ -22,18 +27,27 @@ def pipe():
         key = str(ws)
         clients[key] = ws
         print(key,"add")
+        cur = conn.cursor()
+        #c.execute('SELECT * FROM MESSAGE')
+
+        #for r in c.fetchall():
+        #    print(r)
+
         try:
             while True:
                 msg = ws.receive()
+                #print(msg)
                 d=json.loads(msg)
-
-              
+                print(d)
+                cur.execute("INSERT INTO message VALUES(?,?)",(d['text'],d['date']))
+                conn.commit()
                 for client in clients.values():
                     client.send(d['text'])
         except:
             ws.close()
             del clients[key]
             print(key,"del")
+            
     else:
         return
 
@@ -41,7 +55,7 @@ def main():
     app.debug = True
     host=socket.gethostname()
     ip = socket.gethostbyname(host)
-
+    print(ip)
 
     server = pywsgi.WSGIServer((ip, 8080), app, handler_class=WebSocketHandler)
 
